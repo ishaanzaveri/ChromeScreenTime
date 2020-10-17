4ab4995c19bead841537842a46df6f7492f43aff
anushka48 <70721105+anushka48@users.noreply.github.com>
2020-10-17 17:13:11 -0400

    Update TimeManager.js
    I updated the TimeManagaer.js file by trying to retrieve the URL.

--- TimeManager.js
+++ TimeManager.js
@@ -1,3 +1,31 @@
-chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
+chrome.tabs.onUpdated.addListener((tabID, changeDetails, tab) =>
+{
   console.log(tab);
 });
+var visited = [];
+chrome.tabs.onActivated.addListener(() => {
+// query the active tab
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, activeTab => {
+    // details of the tab
+    console.log(activeTab);
+  });
+});
+const getActiveTab = () => {
+  return new Promise(resolve => {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, activeTab => {
+      resolve(activeTab[0]);
+    });
+  });
+}
+const activeTab = getActiveTab();
+visited.push(activeTab);
+console.log(visited);
+chrome.windows.onFocusChanged.addListener(window => {
+  console.log(window);
+})
