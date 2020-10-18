99633bcd8594c626cf15f004889a14161cbbf650
Ishaan Zaveri <izaveri01@gmail.com>
2020-10-18 14:46:28 +0530

    updated horizontalBar.js

--- chart/horizontalBar.js
+++ chart/horizontalBar.js
@@ -1,22 +1,27 @@
+var ListDict;
+
+chrome.storage.local.get(["data"], (data) => {
+	if (data !== undefined) {
+	  ListDict = data;
+	}
+  });
+  var webNames;
+  var webTimes;
+  for (var key in ListDict) {
+	  webNames.push(key);
+	  webTimes.push(ListDict[key]);
+  }
 let ctx = document.getElementById('myChart').getContext('2d');
 			let chart = new Chart(ctx, {
 				type: 'horizontalBar',
 				data: {
-					labels: [
-						'January',
-						'February',
-						'March',
-						'April',
-						'May',
-						'June',
-						'July',
-					],
+					labels: webNames,
 					datasets: [
 						{
 							label: 'My First dataset',
 							backgroundColor: 'rgb(255, 99, 132)',
 							borderColor: 'rgb(255, 99, 132)',
-							data: [0, 10, 5, 2, 20, 30, 45],
+							data: webTimes,
 						},
 					],
 				},
