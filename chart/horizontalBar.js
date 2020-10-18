89d0a1db2022eb0ddc4cdbbd93600b5657fa953d
Ishaan Zaveri <izaveri01@gmail.com>
2020-10-18 15:09:54 +0530

    push

--- chart/horizontalBar.js
+++ chart/horizontalBar.js
@@ -1,28 +1,36 @@
 var ListDict;
 
-chrome.storage.local.get(["data"], (data) => {
-	if (data !== undefined) {
-	  ListDict = data;
-	}
-  });
-  var webNames;
-  var webTimes;
-  for (var key in ListDict) {
-	  webNames.push(key);
-	  webTimes.push(ListDict[key]);
-  }
-let ctx = document.getElementById('myChart').getContext('2d');
-			let chart = new Chart(ctx, {
-				type: 'horizontalBar',
-				data: {
-					labels: webNames,
-					datasets: [
-						{
-							label: 'My First dataset',
-							backgroundColor: 'rgb(255, 99, 132)',
-							borderColor: 'rgb(255, 99, 132)',
-							data: webTimes,
-						},
-					],
-				},
-			});
+function updateGraph () {
+	chrome.storage.local.get(["data"], (data) => {
+		if (data !== undefined) {
+		  ListDict = data;
+		}
+	  });
+	  var webNames;
+	  var webTimes;
+	  if(ListDict !== undefined){
+		for (var key in ListDict) {
+			webNames.push(key);
+			webTimes.push(ListDict[key]);
+		}
+	  let ctx = document.getElementById('myChart').getContext('2d');
+				  let chart = new Chart(ctx, {
+					  type: 'horizontalBar',
+					  data: {
+						  labels: webNames,
+						  datasets: [
+							  {
+								  label: 'My First dataset',
+								  backgroundColor: 'rgb(255, 99, 132)',
+								  borderColor: 'rgb(255, 99, 132)',
+								  data: webTimes,
+							  },
+						  ],
+					  },
+				  });
+	  } else {
+		  console.log("No data")
+	  }
+	  
+}
+setInterval(updateGraph,5000);
