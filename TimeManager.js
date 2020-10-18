19e7c40ffa23dd1ab0a2f06f43c0e0c33ca37f26
Brian Fernando <brianfdo@users.noreply.github.com>
2020-10-18 04:57:25 -0400

    cleaned up TimeManager.js

--- TimeManager.js
+++ TimeManager.js
@@ -1,7 +1,3 @@
-var visited = [];
-//var visitedParsed = [];
-let counter = 0;
-var ogList = [];
 var ListDict = {};
 var currentTime;
 var currentTabId;
@@ -26,27 +22,10 @@ function parseUrl(url) {
   return (new URL(url)).hostname;
 }
 chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
-  // if (tab.status === "complete"){
-  //   gTab = tab;
-  //   visited.push(tab.url);
-  //   var editedUrl = visited[counter].split("/");
-  //   if (editedUrl[2] !== "newtab") {
-  //     //visitedParsed.push(editedUrl[2]);
-  //     if (!ogList.includes(editedUrl[2])) {
-  //       ogList.push(editedUrl[2]);
-  //       ListDict[editedUrl[2]] = 0;
-  //     }
-  //   }
-  //   console.log(ogList);
-  //   for(var key in ListDict) {
-  //     console.log(key + " : " + ListDict[key] + "First part");
-  //   }
-  //   counter++;
-  // }
   if (tabId !== currentTabId) {
     return;
   }
-  if (onUpdateUrl === undefined || onUpdateUrl === "newtab") {
+  if (onUpdateUrl === undefined) {
     onUpdateUrl = parseUrl(tab.url);
   } else if (parseUrl(tab.url) !== onUpdateUrl) {
     onUpdateUrl = parseUrl(tab.url);
@@ -59,7 +38,6 @@ chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
 });
 
 chrome.tabs.onActivated.addListener(({tabId, windowId}) => {
-  //var tabUrl = chrome.tabs.getTabId();
   currentTabId = tabId;
   chrome.tabs.get(tabId, function(tab) {
     gTab = tab;
@@ -72,27 +50,6 @@ chrome.tabs.onActivated.addListener(({tabId, windowId}) => {
   });
 
 });
-
-// chrome.windows.onFocusChanged.addListener(window => {
-//   counter = 0;
-//   if(gTab != undefined &&  ( window === -1 || window === 1 ) && gTab.status === "complete") {
-//     var editedUrl = gTab.url.split("/");
-
-//   // Date.now() - unix time
-//   if (window === 1 ){
-//     console.log(Date.now());
-//     currentTime = Date.now();
-//   }
-//   if (window === -1) {
-//     ListDict[editedUrl[2]] = (Date.now() - currenttime);
-//     if (ListDict[editedUrl[2]] === NaN) {
-//       console.log(editedUrl[2] + " is NaN " + currenttime)
-//     }
-//   }
-//   console.log("for " + editedUrl[2] + " is " + ListDict[editedUrl[2]])
-//   gTab = undefined;
-// }
-// });
 
 function startTime (url) {
   currentTime = Date.now();
