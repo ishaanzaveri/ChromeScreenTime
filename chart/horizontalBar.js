43481c90fbeb977caaaa815b340735bc784af298
Ishaan Zaveri <izaveri01@gmail.com>
2020-10-18 18:03:33 +0530

    trying to implement only 5 values

--- chart/horizontalBar.js
+++ chart/horizontalBar.js
@@ -5,14 +5,25 @@ function updateGraph () {
 		if (data.data !== undefined) {
 		  ListDict = data.data;
 		}
-	  
-	  var webNames = [];
-	  var webTimes = [];
+		var tempNames = [];
+	  	var tempTimes = [];
+	  	var webNames = [];
+	  	var webTimes = [];
 	  if(ListDict !== undefined){
 		for (var key in ListDict) {
 			webNames.push(key);
 			webTimes.push(ListDict[key]);
 		}
+		for (let index = 0; index < 5; index++) {
+			var max = Math.max(tempTimes); 
+			tempNames[i] = webNames[webTimes.findIndex(max)];
+			tempTimes[i] = webTimes[webTimes.findIndex(max)];
+			for (let index = 0; index < array.length; index++) {
+				const element = array[index];
+				
+			}
+		}
+		
 	  let ctx = document.getElementById('myChart').getContext('2d');
 				  let chart = new Chart(ctx, {
 					  type: 'horizontalBar',
