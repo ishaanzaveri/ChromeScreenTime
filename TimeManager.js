66de1c29cecfeced58f801dc6c9a76ee1b757730
anushka48 <70721105+anushka48@users.noreply.github.com>
2020-10-17 21:07:26 -0400

    Update TimeManager.js
    Parsed the array

--- TimeManager.js
+++ TimeManager.js
@@ -1,10 +1,17 @@
 var visited = [];
-chrome.tabs.onUpdated.addListener((tabID, changeDetails, tab) =>
-{
+var visitedParsed = [];
+chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
   console.log(tab);
   visited.push(tab.url);
   console.log(visited);
+
+  for (var i = 0; i < visited.length; i++) {
+    var editedUrl = visited[i].split("/");
+    visitedParsed.push(editedUrl[2]);
+  }
+  console.log(visitedParsed);
 });
+
 chrome.tabs.onActivated.addListener(() => {
 // query the active tab
   chrome.tabs.query({
@@ -15,6 +22,7 @@ chrome.tabs.onActivated.addListener(() => {
     console.log(activeTab);
   });
 });
+
 chrome.windows.onFocusChanged.addListener(window => {
   console.log(window);
-})
+});
