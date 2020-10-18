d89a58ebd8e83bfcdb6522fc8d28f43a7f47088c
Brian Fernando <brianfdo@users.noreply.github.com>
2020-10-17 22:42:41 -0400

    for loop for dictionary/ excluded "newtab" :)

--- TimeManager.js
+++ TimeManager.js
@@ -2,42 +2,21 @@ var visited = [];
 var visitedParsed = [];
 let counter = 0;
 chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
-  //console.log(tab);
-  // visited = [];
   console.log("Before");
-  //console.log(tab.status);
   if (tab.status === "complete"){
     visited.push(tab.url);
     var editedUrl = visited[counter].split("/");
-    visitedParsed.push(editedUrl[2]);
+    if (editedUrl[2] !== "newtab") {
+      visitedParsed.push(editedUrl[2]);
+    } else {
+      console.log("this is new tab");
+    }
     console.log(visitedParsed);
     counter++;
   }
   console.log("After");
-  
-
-  // console.log(visitedParsed);
-  // counter++;
-  // console.log("counter : " + counter);
-  //console.log(visited);
-
-  // for (var i = 0; i < visited.length; i++) {
-  //   var editedUrl = visited[i].split("/");
-  //   visitedParsed.push(editedUrl[2]);
-  // }
-  // console.log(visitedParsed);
 });
 
-// chrome.tabs.onActivated.addListener(() => {
-// // query the active tab
-//   chrome.tabs.query({
-//     active: true,
-//     currentWindow: true
-//   }, activeTab => {
-//     // details of the tab
-//     console.log(activeTab);
-//   });
-// });
 
 chrome.windows.onFocusChanged.addListener(window => {
   console.log(window);
@@ -47,7 +26,7 @@ chrome.windows.onFocusChanged.addListener(window => {
   //   console.log("run")
   // }
   //for (var i = 0; i < visited.length; i++) {
-  
+
     //console.log("inside for loop: ");
   //}
   //visited = [];
@@ -57,4 +36,4 @@ chrome.windows.onFocusChanged.addListener(window => {
 //   var editedUrl = visited[i].split("/");
 //   visitedParsed.push(editedUrl[2]);
 // }
-// console.log(visitedParsed);
+// console.log(visitedParsed); 
