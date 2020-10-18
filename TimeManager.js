df01b7dd37d29f0a0e9e4c4429b711de9f3b6d44
Ishaan Zaveri <izaveri01@gmail.com>
2020-10-18 10:08:05 +0530

    unix time stuff

--- TimeManager.js
+++ TimeManager.js
@@ -2,7 +2,11 @@ var visited = [];
 var visitedParsed = [];
 let counter = 0;
 var ogList = []; 
+var ListDict = {};
+var currenttime;
+var gTab;
 chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
+  gTab = tab;
   if (tab.status === "complete"){
     visited.push(tab.url);
     var editedUrl = visited[counter].split("/");
@@ -10,31 +14,37 @@ chrome.tabs.onUpdated.addListener((tabId, changeDetails, tab) => {
       visitedParsed.push(editedUrl[2]);
       if (!ogList.includes(editedUrl[2])) {
         ogList.push(editedUrl[2]);
+        ListDict[editedUrl[2]] = 0;
       }
     }
     console.log(ogList);
-    //console.log(visitedParsed);
+    for(var key in ListDict) {
+      console.log(key + " : " + ListDict[key] + "First part");
+    }
     counter++;
   }
 });
 
 
 chrome.windows.onFocusChanged.addListener(window => {
-  console.log(window);
-  // if(window == 1) {
-  //   var editedUrl = visited[0].split("/");
-  //   visitedParsed.push(editedUrl[2]);
-  //   console.log("run")
-  // }
-  //for (var i = 0; i < visited.length; i++) {
-
-    //console.log("inside for loop: ");
-  //}
-  //visited = [];
+  counter = 0; 
+  if(gTab != undefined &&  ( window === -1 || window === 1 )) {
+    var editedUrl = gTab.url.split("/");
+  
+  // Date.now() - unix time
+  if (window === 1 ){
+    console.log(Date.now());
+    currenttime = Date.now();
+  }
+  if (window === -1) {
+    ListDict[editedUrl[2]] = (Date.now() - currenttime); 
+    if (ListDict[editedUrl[2]] === NaN) {
+      console.log(editedUrl[2] + " is NaN " + currenttime)
+    }
+  }
+  console.log("for " + editedUrl[2] + " is " + ListDict[editedUrl[2]])
+  gTab = undefined;
+}
 });
 
-// for (var i = 0; i < visited.length; i++) {
-//   var editedUrl = visited[i].split("/");
-//   visitedParsed.push(editedUrl[2]);
-// }
-// console.log(visitedParsed); 
+ 
