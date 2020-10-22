dfe85f41a5b149fb32f1b378fce1d80cbc914f9c
Brian Fernando <brianfdo@users.noreply.github.com>
2020-10-22 12:29:36 -0400

    Fixed overflow of websites on bar chart

--- chart/horizontalBar.js
+++ chart/horizontalBar.js
@@ -8,26 +8,41 @@ function updateGraph () {
 
 	  var webNames = [];
 	  var webTimes = [];
+	  var finalWebNames = [];
+		var finalWebTimes = [];
 	  if(ListDict !== undefined){
+		webNames = Object.keys(ListDict);
+		webTimes = Object.values(ListDict);
+		webTimes.sort(function(a, b){return b - a});
+		var counter = 0;
 		for (var key in ListDict) {
-			webNames.push(key);
-			webTimes.push(ListDict[key]);
+			if (ListDict[key] === webTimes[counter]) {
+				webNames[counter] = key;
+			}
+			counter = counter + 1;
+		}
+		
+		for (i = 0; i < 5; i++){
+			finalWebNames[i] = webNames[i];
+			finalWebTimes[i] = webTimes[i] / 3600;
 		}
 	  let ctx = document.getElementById('myChart').getContext('2d');
 				  let chart = new Chart(ctx, {
 					  type: 'horizontalBar',
+					  labels: [finalWebNames, 'hours'],
 					  data: {
-						  labels: webNames,
+						  labels: finalWebNames,
 						  datasets: [
 							  {
 								  label: 'Screen Time',
 								  backgroundColor: 'rgb(19, 176, 233)',
 								  borderColor: 'rgb(200, 200, 200)',
-								  data: webTimes,
+								  data: finalWebTimes
 							  },
 						  ],
 					  },
 				  });
+				  ListDict = {};
 	  } else {
 		  console.log("No data")
 	  }
